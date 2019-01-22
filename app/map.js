7b1e789d4d492245423ecd66bb6b9e3c6a6160f4
Viktor Gorte <v_gort01@uni-muenster.de>
2019-01-22 17:50:19 +0100

    test query

--- app/map.js
+++ app/map.js
@@ -13,22 +13,51 @@ layerGroup.addTo(mymap);
 
 var address = "http://giv-oct.uni-muenster.de:8890/sparql?default-graph-uri=http%3A%2F%2Fcourse.geoinfo2018.org%2FG3&format=application/json&timeout=0&debug=on&query=";
 var namespace = "http://course.geoinfo2018.org/G3#";
-var query =  'SELECT DISTINCT * WHERE {?s ?p ?o}LIMIT 10'
+var query =  'PREFIX euwaste: <http://course.geoinfo2018.org/G3#> PREFIX qb: <http://purl.org/linked-data/cube#> PREFIX nuts: <http://rdfdata.eionet.europa.eu/page/ramon/nuts/> SELECT DISTINCT * WHERE {  ?obs a qb:Observation.   ?obs euwaste:refArea nuts:DE.  ?obs euwaste:attrWastePerCapita ?wasteGeneration.}'
+
+
+
+var nut0Codes = [];
+var nut2Codes = [];
 
 
 loadNUTS();
 init();
 
+
+function getAllNuts(){
+    nutsLevel0.features.forEach(element => {
+        nut0Codes.push(element.id)
+    });
+
+    nutsLevel2.features.forEach(element => {
+        nut2Codes.push(element.id)
+    });
+}
+
+function buildQUERY(s, p, o){
+    return "{"+ s +" " + p + " " + o +"}"
+}
+
+
 function init(){
+    getAllNuts()
+    let result = ""
     fetch(address + encodeURIComponent(query))
-        .then(response => response.json())
-            .then(response => console.log(response)
-                );
+        .then(response => {
+            console.log(response)
+            return response.json()
+        })
+        .then(response => {
+            console.log(response)
+        });           
 }
 
 function onclick(e) {
     var nut = e.sourceTarget.feature.id;
     console.log(nut);
+    getAllNuts();
+
 }
   
 function onEachFeature(feature, layer) {
@@ -38,6 +67,7 @@ function onEachFeature(feature, layer) {
 }
 
 function loadNUTS(){
+    
     if(mymap.getZoom() > 6){
         layerGroup.clearLayers();
         geojson = L.geoJson(nutsLevel2, {
@@ -57,11 +87,9 @@ function loadNUTS(){
     }
 }
 
-
 mymap.on('zoomend', function (e) {
     zoom_based_layerchange();
 });
-
 
 function zoom_based_layerchange() {
     console.log(mymap.getZoom());
