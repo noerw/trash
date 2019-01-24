0654f8a50493fe527e657f63034f083f9b0de0d5
Norwin Roosen <github@nroo.de>
2019-01-24 14:26:45 +0100

    refactor layer loading

--- app/map.js
+++ app/map.js
@@ -59,36 +59,54 @@ async function queryTripleStore (query) {
     return json;
 }
 
-
-var maxWaste0;
-var wasteArray0;
-var maxWaste2;
-var wasteArray2;
-
-
 async function initData() {
-    // extract NUTS ids form geojson
-    const nut0Codes = nutsLevel0.features.map(feat => feat.id);
-    const nut2Codes = nutsLevel2.features.map(feat => feat.id);
+    // add NUTS0 choropleth to map
+    const layer0 = await prepareNutsLevel(geodataNuts0);
+    layer0.addTo(layerGroup);
 
-    const waste0 = await fetchWasteData(nut0Codes);
-    maxWaste0 = waste0.maxWaste;
-    wasteArray0 = waste0.wasteGeneration;
-    console.log(waste0)
+    // create placeholder layer for NUTS2, b/c loading the RDF data for NUTS2 takes a while
+    const layer2NoData = L.geoJson(geodataNuts2, {
+        style: {
+            // interactive: false,
+            fillColor: '#666',
+            fillOpacity: 0.4,
+            color: '#fff',
+            weight: 1,
+        },
+        onEachFeature,
+    });
+    let layer2 = layer2NoData
 
-    // apply fetched RDF data to geojson
-    for (const feat of nutsLevel0.features) {
-        feat.properties['wasteGeneration'] = waste0.wasteGeneration[feat.id]
+    // zoom based layer change
+    mymap.on('zoomend', function (e) {
+        layerGroup.clearLayers();
+        if (mymap.getZoom() > 6)
+            layer2.addTo(layerGroup)
+        else
+            layer0.addTo(layerGroup)
+    });
+
+    // replace NUTS2 placeholder with actual chorolpleth layer
+    const layer2WithData = await prepareNutsLevel(geodataNuts2);
+    layerGroup.remove(layer2)
+    layer2 = layer2WithData
+    layer2.addToMap(mymap)
+}
+
+async function prepareNutsLevel (url) {
+    // fetch geojson
+    // const req = await fetch(url)
+    // const geojson = await req.json()
+    const geojson = url
+
+    // apply RDF data to geojson
+    const nutsCodes = geojson.features.map(feat => feat.properties.NUTS_ID);
+    const wasteData = await fetchWasteData(nutsCodes);
+    for (const feat of geojson.features) {
+        feat.properties['wasteGeneration'] = wasteData[feat.properties.NUTS_ID]
     }
 
-
-    //same for nut2
-    // const wast2 = await maxWaste(nut2Codes)
-    // maxWaste2 = waste2.maxWaste;
-    // wasteArray2 = waste2.wasteArray;
-    // console.log(maxWaste2)
-
-    loadNUTS();
+    return buildChoroplethLayer(geojson);
 }
 
 //Asyc fetch latest value for waste for every nut code in set of codes.
@@ -104,131 +122,56 @@ async function fetchWasteData (nutsCodes) {
             wasteGeneration[id] = Number.parseFloat(value);
         }
     }
-    const maxWaste = Math.max.apply(Math, Object.values(wasteGeneration))
-    return { maxWaste, wasteGeneration };
+    return wasteGeneration;
 }
 
-function loadNUTS() {
-    if (mymap.getZoom() > 6) {
-        layerGroup.clearLayers();
-        geojson = L.geoJson(nutsLevel2, {
-            onEachFeature,
-            style: function (feature) {
-                //console.log(feature)
-            }
-        }).addTo(layerGroup);
-    } else {
-        geojson = L.choropleth(nutsLevel0, {
-            valueProperty: 'wasteGeneration', // geojson properties property
-            scale: ['yellow', 'red'], // chroma.js scale - include as many as you like
-            steps: 15,
-            mode: 'k', // q for quantile, e for equidistant, k for k-means
-            onEachFeature,
-            style: {
-                color: '#fff', // border color
-                weight: 2,
-                fillOpacity: 0.8
-            },
-        }).addTo(mymap)
-    }
-}
-
-//#################################
-//#### Map Stuff and coloring #####
-//#################################
-
-async function onclick(e) {
-    var nut = e.sourceTarget.feature.id;
-    var name = e.sourceTarget.feature.properties.NUTS_NAME.toLowerCase();
-    name = name.charAt(0).toUpperCase() + name.slice(1);
-    document.getElementById('dataTitle').innerHTML = name;
-    openNav();
-
-    const json = await queryLatestWasteGenForNuts(nut);
-
-    const pieData = await queryLatestForNuts(nut);
-
-    setLineChart(json.results.bindings);
-
-    setPrcChart(pieData.results.bindings[0]);
-}
-
-function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: onclick
-    });
-}
-
-function resetHighlight(e) {
-    geojson.resetStyle(e.target);
-}
-
-function highlightFeature(e) {
-    var layer = e.target;
-
-    layer.setStyle({
-        weight: 5,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
-    });
-
-    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-        layer.bringToFront();
-    }
-}
-mymap.on('zoomend', function (e) {
-    zoom_based_layerchange();
-});
-
-function zoom_based_layerchange() {
-    loadNUTS();
-}
-
-function style(feature) {
-    const region = wasteArray0.find(elem => {
-        if (elem.nut == feature.id) return elem;
-    })
-    if (region) {
-        const normalizedWaste = region.waste / maxWaste0;
-
-        return {
-            fillColor: getColor(normalizedWaste),
-            weight: 2,
-            opacity: 1,
-            color: 'white',
-            dashArray: '3',
+function buildChoroplethLayer (geojson) {
+    return L.choropleth(geojson, {
+        valueProperty: 'wasteGeneration', // geojson properties property
+        scale: ['yellow', 'red'], // chroma.js scale - include as many as you like
+        steps: 15,
+        mode: 'k', // q for quantile, e for equidistant, k for k-means
+        style: {
+            color: '#fff', // border color
+            weight: 1,
             fillOpacity: 0.7
-        };
-    }
+        },
+        onEachFeature,
+    })
 }
 
-function getColor(d) {
-    return  d >= 1 ? '#E500AD' :
-            d > .9 ? '#CE13A0' :
-            d > .8 ? '#B72693' :
-            d > .7 ? '#A03986' :
-            d > .6 ? '#894C7A' :
-            d > .5 ? '#725F6D' :
-            d > .4 ? '#5B7260' :
-            d > .3 ? '#448554' :
-            d > .2 ? '#2D9847' :
-            d > .1 ? '#16AB3A' :
-            d == undefined ? '#000000':
-                     '#00BF2E'
+function onEachFeature (feature, layer) {
+    layer.on({
+        mouseover: (event) => {
+            event.target.setStyle({
+                weight: 3,
+                color: '#666',
+            }).bringToFront();
+        },
+        mouseout: (event) => {
+            event.target.setStyle({
+                weight: 1,
+                color: '#fff',
+            }).bringToFront();
+        },
+        click: async (event) => {
+            const nut = event.sourceTarget.feature.id;
+            document.getElementById('dataTitle').innerHTML = nut;
+            openNav();
+            const json = await queryLatestWasteGenForNuts(nut)
+            setLineChart(json.results.bindings);
+            setPrcChart(json.results.bindings);
+        },
+    });
 }
-
 
 function openNav() {
-  document.getElementById("main").style.marginRight = "25%";
-  document.getElementById("mySidenav").style.width = "25%";
-  document.getElementById("mySidenav").style.display = "block";
-
+    document.getElementById("main").style.marginRight = "25%";
+    document.getElementById("mySidenav").style.width = "25%";
+    document.getElementById("mySidenav").style.display = "block";
 }
 
 function closeNav() {
-document.getElementById("main").style.marginRight = "0%";
-document.getElementById("mySidenav").style.display = "none";
+    document.getElementById("main").style.marginRight = "0%";
+    document.getElementById("mySidenav").style.display = "none";
 }
