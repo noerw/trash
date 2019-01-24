713232bf783c336d4111b258f692ed6a88c9fdbc
Viktor Gorte <v_gort01@uni-muenster.de>
2019-01-24 14:45:37 +0100

    legend title

--- app/map.js
+++ app/map.js
@@ -116,7 +116,7 @@ function buildLegend(layer){
       var labels = []
 
       // Add min & max
-      div.innerHTML = '<div class="labels"><div class="min">' + limits[0] + '</div> \
+      div.innerHTML = 'Waste Generation in kg/Person<div class="labels"><div class="min">' + limits[0] + '</div> \
               <div class="max">' + limits[limits.length - 1] + '</div></div>'
 
       limits.forEach(function (limit, index) {
